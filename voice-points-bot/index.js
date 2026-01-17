f0829a7bfcc757ca42d556a74c157fdc07144299
bot <naif55areeg@gmail.com>
2026-01-17 21:08:43 +0300

    first commit

---
+++ voice-points-bot/index.js
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
